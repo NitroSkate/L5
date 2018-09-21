ef354b45287df02ee703e616899577fb8711e13e
Joshua <00159817@uca.edu.sv>
2018-09-21 17:13:49 -0600

    Terminado

--- Ejercicio_2.js
+++ Ejercicio_2.js
@@ -1,17 +1,16 @@
 function ordena(c){
-    for(let i=1; i<c.length-1; i++){
+    for(let i=1; i<c.length; i++){
         k=c[i];
         j=i-1;
-        while (j>0 && k<c[j]){
+        while (j>-1 && k<c[j]){
             c[j+1] = c[j];
-            j=j-1;
-        c[j+1]=k;
+            j=j-1;     
         }
+        c[j+1]=k;
     }
     return c;
 }
 
 
 c=prompt('Ingrese numeros: ');
-c.split("");
-alert(ordena(c));
+console.log(ordena(c.split("")));
