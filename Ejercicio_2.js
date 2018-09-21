15449b5f7ca4f3bb715f55a1adf4c9d01365388b
Joshua <00159817@uca.edu.sv>
2018-09-21 17:06:29 -0600

    seh

--- Ejercicio_2.js
+++ Ejercicio_2.js
@@ -1,5 +1,13 @@
 function ordena(c){
-    c.sort;
+    for(let i=1; i<c.length-1; i++){
+        k=c[i];
+        j=i-1;
+        while (j>0 && k<c[j]){
+            c[j+1] = c[j];
+            j=j-1;
+        c[j+1]=k;
+        }
+    }
     return c;
 }
 
